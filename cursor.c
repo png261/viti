97652e890147b842ad774f590f52746a6cb4ccb7
png216 <nhphuong.code@gmail.com>
2022-08-09 15:16:48 +0700

    update scroll

--- cursor.c
+++ cursor.c
@@ -12,39 +12,64 @@ void cursor_refresh(Buffer *buf) {
     wrefresh(buf->win);
 }
 
-void cursor_right(Buffer *buf) {
-    if (buf->cur.x + 1 >= buf->view.x) {
-        buf->view.xoff =
-            MIN(buf->view.xoff++, buf->rows[buf->cur.y].size - buf->view.x);
-        buffer_render_rows(buf);
+void cursor_check(Buffer *buf) {
+    Row *row = &buf->rows[buf->view.line];
+    buf->cur.x = RANGE(buf->cur.x, 0,
+                       (row->size < buf->view.x ? row->size : buf->view.x - 1));
+    buf->cur.y = RANGE(buf->cur.y, 0, MIN(buf->file.lines, buf->view.y - 1));
+}
+
+void handleScroll(Buffer *buf) {
+    Row *row = &buf->rows[buf->view.line];
+    if (buf->cur.x < 0) {
+        buf->view.xoff--;
     }
-    buf->cur.x = MIN(buf->cur.x++, buf->view.x - 1);
+
+    if (buf->cur.x >= buf->view.x) {
+        buf->view.xoff += buf->cur.x + 1 - buf->view.x;
+    }
+
+    if (buf->cur.y < 0) {
+        buf->view.yoff--;
+    }
+
+    if (buf->cur.y >= buf->view.y) {
+        buf->view.yoff += buf->cur.y + 1 - buf->view.y;
+    }
+
+    buf->view.xoff =
+        RANGE(buf->view.xoff, 0,
+              row->size < buf->view.x ? 0 : row->size - buf->view.x);
+    buf->view.yoff = RANGE(
+        buf->view.yoff, 0,
+        buf->file.lines < buf->view.y ? 0 : buf->file.lines - buf->view.y);
+    buffer_render_rows(buf);
+}
+
+void cursor_right(Buffer *buf) {
+    buf->cur.x++;
+    handleScroll(buf);
+    cursor_check(buf);
     cursor_refresh(buf);
 }
 
 void cursor_left(Buffer *buf) {
-    if (buf->cur.x <= 0) {
-        buf->view.xoff = MAX(buf->view.xoff--, 0);
-        buffer_render_rows(buf);
-    }
-    buf->cur.x = MAX(buf->cur.x--, 0);
+    buf->cur.x--;
+    handleScroll(buf);
+    cursor_check(buf);
     cursor_refresh(buf);
 }
 
 void cursor_up(Buffer *buf) {
-    if (buf->cur.y <= 0) {
-        buf->view.yoff = MAX(buf->view.yoff--, 0);
-        buffer_render_rows(buf);
-    }
-    buf->cur.y = MAX(buf->cur.y--, 0);
+    buf->cur.y--;
+    handleScroll(buf);
+    cursor_check(buf);
     cursor_refresh(buf);
 }
 
 void cursor_down(Buffer *buf) {
-    if (buf->cur.y + 1 >= buf->view.y) {
-        buf->view.yoff = MIN(buf->view.yoff++, buf->file.lines - buf->view.y);
-        buffer_render_rows(buf);
-    }
-    buf->cur.y = MIN(buf->cur.y++, buf->view.y - 1);
+    buf->cur.y++;
+    handleScroll(buf);
+    cursor_check(buf);
     cursor_refresh(buf);
 }
--- util.h
+++ util.h
@@ -1,5 +1,6 @@
 #define MAX(A, B)               ((A) > (B) ? (A) : (B))
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#define RANGE(X,MIN, MAX)       ((X) < (MIN) ? (MIN) :(X) > (MAX) ? (MAX) : (X))
 
 void die(const char *fmt, ...);
 void quit();
